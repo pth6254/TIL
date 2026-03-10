46355ebeda7a19d20b4b41ed891f76fdf2b03f50
pth6254 <pth121819@gmail.com>
2026-03-10 17:50:04 +0900

    "파일 추가"

---
+++ 05-langgraph/05_rag/tools.py
@@ -0,0 +1,11 @@
+# Tools
+from langchain.tools import tool
+from vectorstore import retriever
+
+@tool
+def retrieve_blog_posts(query: str) -> str:
+    '''AI, LLM에 관련된 정보를 블로그 글에서 검색 후 return'''
+    docs = retriever.invoke(query)
+    # docs는 메타데이터를 포함한 리스트 -> LLM은 내용만 들어있는 str이면 충분하다.
+    # 전처리 작업
+    return '\n\n'.join([doc.page_content for doc in docs])
---
+++ 06-deepagent/state.py
@@ -0,0 +1,22 @@
+# state.py
+from langgraph.graph import MessagesState
+
+class State(MessagesState):
+    # messages
+    files: list
+    upload_paths: list
+
+
+'''
+{
+    'files': ['a.csv', 'b.py'],
+    'upload_paths': [
+        '/home/daytona/data/a.csv',
+        '/home/daytona/data/b.py',
+    ],
+    'messages': [
+        HumanMessage(content='hi'),
+        AIMessage(content='hello')
+    ]
+}
+'''
